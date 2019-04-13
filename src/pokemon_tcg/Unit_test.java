5da5424716cbd7a6186cac13f19be7a70cb8ef0e
Jalbornozv <julio.albornoz@ug.uchile.cl>
2019-04-13 12:33:14 -0400

    Initial commit

---
+++ src/pokemon_tcg/Unit_test.java
@@ -0,0 +1,14 @@
+package pokemon_tcg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Unit_test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
